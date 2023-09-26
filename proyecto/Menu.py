eb9a3f2562cb914fbc3a0446e6c019e2e7a956c3
SilenXz <jackbyebyjack@gmail.com>
2023-09-26 22:47:37 +0200

    Menu mejorado

--- proyecto/Menu.py
+++ proyecto/Menu.py
@@ -1,22 +1,42 @@
 import pygame, random
 
-ROJO = (255, 0, 0)
-
 pygame.init()
 screen_width = 800
 screen_height = 600
+
 pantalla = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Hola")
+pygame.display.set_caption("Menu")
+
+juego_inicio = False
+
+font = pygame.font.SysFont("arialblack", 40)
+
+color_text = (255, 255, 255)
+
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, color_text)
+    pantalla.blit(img, (x, y))
+
 
 run = True
-
 while run:
-    pantalla.fill((5, 130, 250))
-    pygame.draw.rect(pantalla, ROJO, (300, 350, 200, 100))
+
+    pantalla.fill((202, 228, 241))
+
+    if juego_inicio == True:
+        pass
+    else:
+        draw_text("Pulsa Espacio para Comenzar", font, color_text, 70, 250)
+
+
 
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            run=False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                juego_inicio = True
+        if event.type == pygame.QUIT:
+            run = False
 
     pygame.display.update()
 
