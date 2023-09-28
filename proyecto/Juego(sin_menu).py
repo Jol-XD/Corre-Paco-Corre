faec8cfb11564e09d882cd96019225e777c21d69
Joel <jolpf777@gmail.com>
2023-09-28 21:02:24 +0200

    salto avanzado (ahun hay problemas)

--- proyecto/Juego(sin_menu).py
+++ proyecto/Juego(sin_menu).py
@@ -8,18 +8,34 @@ FONDO = (5, 130, 250)
 class Jugador(pygame.sprite.Sprite):
     def __init__(self, x, y, velocity_x, velocity_y):
         super().__init__()
-        self.position = (x, y)
-        self.velocity = (velocity_x, velocity_y)
+        self.position = [x, y]
+        self.velocity = [velocity_x, velocity_y]
+        self.is_jumping = False
+        self.jump_strength = -10
+        self.gravity = 0.5
         
     def update(self):
-        self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
-    
-    def draw(self, surface):
-        pygame.draw.circle(surface, (255, 0, 0), self.position, 10)
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
+
+        if not self.is_jumping:
+            self.velocity[1] += self.gravity
+
+        if self.position[1] >= 600 - 10:
+            self.is_jumping = False 
+            self.position[1] = 600 - 10
+            self.velocity[1] = 0
 
     def salto(self):
-        self.velocity = (1, self.velocity[0])
-
+        if not self.is_jumping:
+            self.is_jumping = True
+            self.velocity[1] = self.jump_strength
+        
+    
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255, 0, 0), (int(self.position[0]), int(self.position[1])), 10)
+        
+        
 pygame.init()
 screen_width = 800
 screen_height = 600
@@ -39,15 +55,13 @@ while run:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            run = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 jugador.salto()
-                #pantalla.fill(FONDO)
-                #pygame.draw.rect(pantalla, (28, 133, 45), (0, 500, 800, 200))
-                #pygame.draw.rect(pantalla, ROJO, (250, 200, 100, 200))
                 print("salto")
+                
         
     jugador.update()
 
