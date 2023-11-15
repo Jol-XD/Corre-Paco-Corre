a0f72aa669094b00e4443d51a3a0b092c0993c7a
Joel <jolpf777@gmail.com>
2023-11-15 20:17:33 +0100

    el pasado te hace pensar

--- proyecto/Juego (test-Joel).py
+++ proyecto/Juego (test-Joel).py
@@ -1,15 +1,15 @@
 import random
 import time
 import pygame
-import os
 
-clock = pygame.time.Clock()
-OFFSET = 10 
+clock = pygame.time.Clock()  
+
 ROJO = (255, 0, 0)
 VERDE = (28, 121, 28)
 AZUL = (0, 0, 255)
 MARRON = (128, 64, 0)
 FONDO = (5, 130, 250)
+
 
 pygame.init()
 SCREEN_WIDTH = 1200
@@ -17,28 +17,16 @@ SCREEN_HEIGHT = 900
 pantalla = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("¡Corre Paco corre!")
 
-# Carga la imagen del suelo original
-original_suelo_image = pygame.image.load(os.path.join("proyecto", "sprites", "cesped.png")).convert_alpha()
-
-# Redimensiona la imagen para que tenga el mismo ancho que la pantalla
-suelo_width = SCREEN_WIDTH
-suelo_height = int(original_suelo_image.get_height() * (suelo_width / original_suelo_image.get_width()))
-suelo_image = pygame.transform.scale(original_suelo_image, (suelo_width, suelo_height))
-# Define the player class
+#Define al jugador
 class Jugador(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, velocity_x, velocity_y):
         super().__init__()
-        original_image = pygame.image.load(os.path.join("proyecto", "sprites", "pibe_palo.png")).convert_alpha()
-        self.image = pygame.transform.scale(original_image, (width, height))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect = pygame.Rect(x, y, width, height)
         self.velocity = [velocity_x, velocity_y]
         self.is_jumping = False
         self.is_agachado = False
         self.gravity = 1.1
         self.jump_strength = -20
-        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
         self.rect.x += self.velocity[0]
@@ -76,45 +64,41 @@ class Jugador(pygame.sprite.Sprite):
         if self.is_agachado:
             self.is_agachado = False
             self.rect.height = 80
-            self.rect.y -= 0
+            self.rect.y -= 0 
+
 
     def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        pygame.draw.rect(surface, ROJO, self.rect)
 
-class Structure(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, velocity):
+class Estructura(pygame.sprite.Sprite):
+    def __init__(self, x, width, height, velocity):
         super().__init__()
-        self.image = pygame.image.load(os.path.join("proyecto", "sprites", "structuras", "structure1(big).png")).convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y + self.get_height()
+        self.rect = pygame.Rect(x, 0, width, height)  
         self.velocity = velocity
-        self.mask = pygame.mask.from_surface(self.image)
-
-    def get_height(self):
-        return self.rect.height
-
+    
     def update(self):
         self.rect.x -= self.velocity
         if self.rect.right < 0:
             self.rect.x = SCREEN_WIDTH + 200
+            stucture_sel = random.randint(1, 2)
+            if stucture_sel==1:
+                self.rect.y = SCREEN_HEIGHT - self.rect.height - 100
+            elif stucture_sel==2:
+                self.rect.y = 720 - self.rect.height
             self.velocity += 1
-
-
-
-jugador = Jugador(320, 700, 40, 80, 0, 0)
-# Agrégala al grupo de sprites
-todos_los_sprites = pygame.sprite.Group()
+                
+        
+jugador = Jugador(320, 240, 40, 80, 0, 0)
 
 estructuras = pygame.sprite.Group()
 
 for _ in range(1):
-    nueva_estructura = Structure(random.randint(SCREEN_WIDTH, SCREEN_WIDTH + 200), 0, 50, 120, 10)
-    nueva_estructura.rect.y = SCREEN_HEIGHT - nueva_estructura.get_height() - 121
+    nueva_estructura = Estructura(random.randint(SCREEN_WIDTH, SCREEN_WIDTH + 200), 50, 120, 10)
     estructuras.add(nueva_estructura)
 
 run = True
 perder = False
+
 
 while run:
     for event in pygame.event.get():
@@ -122,39 +106,29 @@ while run:
             run = False
 
         if event.type == pygame.KEYDOWN:
+             #Si pulsas la flecha arriba sasltas
             if event.key == pygame.K_UP:
                 jugador.salto()
                 print("salto")
-            if event.key == pygame.K_DOWN and not jugador.is_jumping:
+            #Si pulsas la flecha abajo te agachas
+            if event.key == pygame.K_DOWN:
                 jugador.agacharse()
                 print("agachado")
 
+        #Si sueltas la flecha abajo te levantas
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
                 jugador.levantarse()
                 print("levantado")
-
-    choque = pygame.sprite.spritecollide(jugador, estructuras, False, pygame.sprite.collide_mask)
+        
+    choque = pygame.sprite.spritecollide(jugador, estructuras, False)
     if choque:
-        lowest_collision_y = max([estructura.rect.top for estructura in choque])
-
-        if jugador.rect.bottom <= lowest_collision_y + OFFSET:
-            if jugador.velocity[1] > 0:
-                jugador.rect.bottom = lowest_collision_y + OFFSET
-                jugador.velocity[1] = 0
-            jugador.is_jumping = False
-        else:
-            # Si el jugador está descendiendo, dejarlo atravesar la plataforma
-            if jugador.velocity[1] > 0:
-                jugador.is_jumping = True
-                jugador.rect.y = lowest_collision_y - jugador.rect.height
-            else:
-                # Si el jugador está subiendo, detenerlo
-                jugador.rect.y = lowest_collision_y + 1
-                jugador.velocity[1] = 0
-
+        estructura_colisionada = choque[0]  # Obtén la primera estructura con la que ha colisionado
+        if jugador.rect.right > estructura_colisionada.rect.left:
+            jugador.rect.right = estructura_colisionada.rect.left
+            
     if jugador.rect.right < 0:
-        run = False
+        run=False
 
     jugador.update()
     estructuras.update()
@@ -162,16 +136,15 @@ while run:
     pantalla.fill(FONDO)
 
     for estructura in estructuras:
-        pantalla.blit(estructura.image, estructura.rect)
+        pygame.draw.rect(pantalla, MARRON, estructura.rect)
 
     jugador.draw(pantalla)
 
-    pygame.draw.rect(pantalla, VERDE, pygame.Rect(0, 780, 1200, 121))
+    pygame.draw.rect(pantalla, VERDE, pygame.Rect(0, 780, 1200, 500))
 
-    todos_los_sprites.draw(pantalla)
+    pygame.display.flip()  # Actualizar la pantalla
+    clock.tick(60)  # Limitar los FPS a 60
 
-
-    pygame.display.flip()
-    clock.tick(60)
+    pygame.display.update()
 
 pygame.quit()
